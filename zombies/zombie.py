ce1a1c42ea18888872535396e7e4db5ddb01114c
DeathLook3421 <159024623+DeathLook3421@users.noreply.github.com>
2024-03-13 08:05:37 -0500

    Add files via upload

---
+++ zombies/zombie.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.sprite import Sprite
+from pygame import *
+import util
+
+
+class Zombie(Sprite):
+    def __init__(self):
+        self.imagenes = [util.cargar_imagen('imagenes/z0.jpg'),
+                                        util.cargar_imagen('imagenes/z1.jpg'),
+                                        util.cargar_imagen('imagenes/z2.jpg'),
+                                        util.cargar_imagen('imagenes/z3.jpg'),
+                                        util.cargar_imagen('imagenes/z4.jpg')]
+        self.cont = 0
+        self.image = self.imagenes[self.cont]
+        self.rect = self.image.get_rect()
+        self.rect.move_ip(10, 325)
+        self.vel = (2,3)
+        self.vida = 100
+        self.caminar = pygame.mixer.Sound('sonido/caminar.mp3')
+        self.caminar.set_volume(0.1)
+        
+    def update(self,size):
+
+        self.caminar.play()
+        self.rect.x = (self.rect.x - self.vel[0]) % size[0]
+        self.cont = (self.cont + 1) % 4
+        self.image = self.imagenes[self.cont]
+    
+        self.caminar.play()
+        self.cont = (self.cont + 1) % 4
+        self.image = self.imagenes[self.cont]
+        self.rect.x = (self.rect.x + self.vel[0]) % size[0]
